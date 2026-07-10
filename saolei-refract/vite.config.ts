c90da34ce36a2becd620e234c3d93872ee1a6e03
Radwimps <67226385+zhouhaoyiu@users.noreply.github.com>
2026-07-11 05:34:45 +0800

    build: upgrade pnpm app stacks

--- saolei-refract/vite.config.ts
+++ saolei-refract/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
@@ -15,9 +15,7 @@ export default defineConfig({
     },
   },
   plugins: [
-    Vue({
-      reactivityTransform: true,
-    }),
+    Vue(),
 
     // https://github.com/hannoeru/vite-plugin-pages
     Pages(),
